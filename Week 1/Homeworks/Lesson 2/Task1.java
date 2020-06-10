fb0a915f2910eecad7043f9a71207408d0af6348
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-11 00:17:52 +0300

    Create Task1.java

---
+++ Week 1/Homeworks/Lesson 2/Task1.java
@@ -0,0 +1,27 @@
+//  github.com/DaniAngelov
+
+import java.util.Scanner;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Въведете А:");
+		float a = sc.nextFloat();
+		
+		System.out.println("Въведете B:");
+		float b = sc.nextFloat();
+		
+		System.out.println("Въведете C:");
+		float c = sc.nextFloat();
+		
+		if(c > a && c < b)
+		{
+			
+			System.out.println("Числото " + c + " е между " + a + " и " + b);
+		}
+		
+		
+
+	}
