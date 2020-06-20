d47dcd9e1e628b8a61f6aa0dcf44cce62588669e
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-21 01:15:21 +0300

    Create Task18.java

---
+++ Week 1/Homeworks/Lesson 3/Task18.java
@@ -0,0 +1,35 @@
+//  github.com/DaniAngelov
+
+import java.util.Scanner;
+
+
+
+public class Task1 
+{
+	
+	static boolean check(int num)
+	{
+		return num % 50 == 0;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a < 1 || a > 9 || b < 1 || b > 9){
+			System.out.println("Invalid args!");
+			return;
+		}
+		
+		for(int i = 1;i <= a;i++)
+		{
+			for(int j = 1;j <= b;j++)
+			{
+				System.out.println(i + "*" + j + "= " + (i * j));
+			}
+		}
+		
+	}
+	
+}
