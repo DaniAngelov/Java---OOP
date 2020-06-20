5498a792c6b82e51e03cff1cbd3c4a116b4aac46
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-21 00:49:54 +0300

    Create Task12.java

---
+++ Week 1/Homeworks/Lesson 3/Task12.java
@@ -0,0 +1,28 @@
+//  github.com/DaniAngelov
+
+import java.util.Scanner;
+
+public class Task1 
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		//int n = sc.nextInt();
+		
+		
+		for(int i = 120; i <= 999;i++)
+		{
+			int lastdigit = i % 10;
+			int firstdigit = i / 100;
+			int middledigit = (i / 10) % 10;
+			
+			if(firstdigit != middledigit && middledigit != lastdigit
+					&& lastdigit != firstdigit)
+			{
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
