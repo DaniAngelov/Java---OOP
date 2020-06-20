b1c9a8ef2ed3f607331a1cdcc59665899b00c7d6
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-21 01:11:42 +0300

    Create Task17.java

---
+++ Week 1/Homeworks/Lesson 3/Task17.java
@@ -0,0 +1,45 @@
+//  github.com/DaniAngelov
+
+import java.util.Scanner;
+
+
+
+public class Task1 
+{
+	
+	static boolean check(int num)
+	{
+		return num % 50 == 0;
+	}
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		char sym = sc.next().charAt(0);
+		
+		for(int i = 0;i <n;i++)
+		{
+			for(int j = 0;j < n;j++)
+			{
+				if(i == 0 || i == n - 1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					if(j == 0 || j == n - 1)
+					{
+						System.out.print("*");
+					}
+					else
+						System.out.print(sym);
+				}
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
+}
