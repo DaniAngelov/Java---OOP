ce4e77a242987843257e9fcfe0626058632ae659
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-20 23:49:41 +0300

    Create Task6.java

---
+++ Week 1/Homeworks/Lesson 3/Task6.java
@@ -0,0 +1,23 @@
+//  github.com/DaniAngelov
+
+import java.util.Scanner;
+
+public class Task1 
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int sum =0;
+		for(int i = 1;i <= 7;i++)
+		{
+			sum += i;
+		}
+		
+		System.out.println(sum);
+		
+		
+	
+	}
+}
