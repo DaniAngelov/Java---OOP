2d3ab86b63434e3ad38fe08f8b582d84b7e736a2
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-20 23:43:12 +0300

    Create Task2.java

---
+++ Week 1/Homeworks/Lesson 3/Task2.java
@@ -0,0 +1,17 @@
+//  github.com/DaniAngelov
+
+import java.util.Scanner;
+
+public class Task1 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i = -20;i <= 50;i++)
+		{
+			System.out.println(i);
+		}
+		
+	
+	}
+}
