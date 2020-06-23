5dc266c037bc41c5ea8c3abc2a61fc19f2cd77eb
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-06-23 19:39:02 +0300

    Create Task5.java

---
+++ Week 2/Homework/Task5.java
@@ -0,0 +1,23 @@
+
+//  github.com/DaniAngelov
+
+import java.util.Scanner;
+
+public class Task1 
+{
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[10];
+		
+		for(int i = 0;i < arr.length;i++)
+		{
+			arr[i] = i * 3;
+			System.out.print(arr[i] + " ");
+		}
+		
+		
+	}
+	
+}
