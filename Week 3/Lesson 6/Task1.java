130e036495c5f0d941c4b0312cecb41f678b5399
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-07-22 15:19:04 +0300

    Update Task1.java

--- Week 3/Lesson 6/Task1.java
+++ Week 3/Lesson 6/Task1.java
@@ -1,1 +1,46 @@
+//  github.com/DaniAngelov
 
+
+import java.util.Scanner;
+
+public class Task12 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int arr[][] = new int[N][M];
+
+        for(int i =0;i < N;i++)
+        {
+            for(int j = 0;j < M;j++)
+            {
+                arr[i][j] = sc.nextInt();
+
+            }
+        }
+
+        int min = arr[0][0];
+        int max = 0;
+
+        for(int i =0;i < N;i++)
+        {
+            for(int j = 0;j < M;j++)
+            {
+                if(arr[i][j] < min)
+                    min = arr[i][j];
+
+                if(arr[i][j] > max)
+                    max = arr[i][j];
+
+            }
+        }
+
+        System.out.println("Biggest: " + max);
+        System.out.println("Smallest: " + min);
+
+
+    }
+}
