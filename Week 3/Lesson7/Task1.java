d8f639096291ba553ce14dd5f6baf2b4d3fc97e8
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-07-23 22:52:54 +0300

    Update Task1.java

--- Week 3/Lesson7/Task1.java
+++ Week 3/Lesson7/Task1.java
@@ -1,1 +1,30 @@
+//  github.com/DaniAngelov
 
+import java.util.Scanner;
+
+public class Task12 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        boolean check = true;
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = sc.nextInt();
+            if(arr[i] < 0)
+                check =false;
+        }
+
+        if(check == true)
+        {
+            System.out.println("Всички числа са неотрицателни");
+        }
+        else
+        {
+            System.out.println("НЕ всички числа са неотрицателни");
+        }
+    }
+}
