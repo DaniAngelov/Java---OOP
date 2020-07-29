d9a800a6761a53b2dae1430935eea7d8a0abd555
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-07-29 16:13:59 +0300

    Update Task1.java

--- Week 3/Lesson 8/Task1.java
+++ Week 3/Lesson 8/Task1.java
@@ -1,1 +1,28 @@
+//  github.com/DaniAngelov
 
+import java.util.Scanner;
+
+
+public class Task1 {
+
+    public static void printSent(String sent)
+    {
+        sent = sent.toUpperCase();
+        System.out.print(sent + " ");
+        sent = sent.toLowerCase();
+        System.out.print(sent + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String sent = sc.nextLine();
+        String sent2 = sc.nextLine();
+
+        if(sent.length() > 40)
+        {
+            return;
+        }
+        printSent(sent);
+        printSent(sent2);
+    }
+}
